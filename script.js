3107673c6761c6006fafa0f631a41aa6423a33f7
Code4426Josh <chukwuebukajosh8@gmail.com>
2023-09-07 23:51:14 +0100

    building page

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+/* -------------------------------- cat fact -------------------------------- */
+// const display = document.querySelector('.show');
+// const button = document.querySelector('.btn');
 
-const display = document.querySelector('.show');
-const button = document.querySelector('.btn');
+// const getItems = async () => {
+//   const response = await fetch('https://catfact.ninja/fact');
+//   const result = await response.json();
 
-const getItems = async () => {
-  const response = await fetch('https://catfact.ninja/fact');
-  const result = await response.json();
+//   display.textContent = result.fact;
+// };
 
-  display.textContent = result.fact;
-};
+// const handleClick = () => {
+//   getItems();
+// }
 
-const handleClick = () => {
-  getItems();
-}
+/* ----------------------------- college search ----------------------------- */
+
