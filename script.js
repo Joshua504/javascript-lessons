9681ce9293dccf20a6eb5d280054bd23bd2d8286
Code4426Josh <chukwuebukajosh8@gmail.com>
2023-09-10 23:47:21 +0100

    set some limit on the returned data

--- script.js
+++ script.js
@@ -15,3 +15,32 @@
 
 /* ----------------------------- college search ----------------------------- */
 
+const deletEl = document.querySelector('.trash')
+const bodyEl = document.querySelector('.cont-container')
+console.log('bodyEl: ', bodyEl);
+
+
+window.addEventListener('load', async function () {
+  await fetch('http://universities.hipolabs.com/search?country=United+States&limit=100')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(school => {
+        bodyEl.innerHTML += `   
+      <section class="content">
+        <div>
+          <h3>name - <span>${school.name}</span></h3>
+          <h3>website - <a href=""></a></h3>
+          <h3>domain - <span></span></h3>
+          <h3>country - <span></span></h3>
+        </div>
+        <div>
+          <ion-icon class="trash" name="trash-outline"></ion-icon>
+        </div>
+      </section>`
+      });
+    });
+  });
+
+function getAllSchool() {
+  
+}
