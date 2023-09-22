33116268c0cdb656c55cb7536709a1895089b3a6
Code4426Josh <chukwuebukajosh8@gmail.com>
2023-09-22 21:20:37 +0100

    handled the search filter feature

--- script.js
+++ script.js
@@ -17,12 +17,19 @@
 
 const deletEl = document.querySelectorAll('.trash')
 const bodyEl = document.querySelector('.cont-container')
-const inputEl = document.querySelector('.inpt')
+
+let fixedData = [];
 
 window.addEventListener('load', async function () {
-  await fetch('http://universities.hipolabs.com/search?country=United+States&limit=10')
+  await fetch('http://universities.hipolabs.com/search?country=United+States&limit=20')
     .then(response => response.json())
     .then(data => {
+      //save to a fixed arrray for search
+      data.forEach(single => {
+        fixedData.push(single)
+      })
+
+      //show fetched data
       data.forEach(school => {
         bodyEl.innerHTML += `   
       <section class="content">
@@ -43,7 +50,36 @@ window.addEventListener('load', async function () {
       });
     });
 });
-  
+
+function handleSearch() {
+  console.log('fixedData: ', fixedData);
+  const inputEl = document.querySelector('.search')
+  console.log('inputEl: ', inputEl.value);
+  let search = fixedData.filter((element) => element.name.includes(inputEl.value));
+  console.log('search: ', search);
+  bodyEl.innerHTML = '';
+  search.forEach(school => {
+    bodyEl.innerHTML += `
+      <section class="content">
+        <div>
+          <h3>name - <span>${school.name}</span></h3>
+          <h3>website -
+            <a href="${school.web_pages}"target="blank">
+              ${school.web_pages}
+            </a>
+          </h3>
+          <h3>domain - <span>${school.domains}</span></h3>
+          <h3>country - <span>${school.country}</span></h3>
+        </div>
+        <div>
+          <ion-icon class="trash" name="trash-outline"></ion-icon>
+        </div>
+      </section>`
+  });
+}
+
+// handleSearch();
+
 function deleteData(element) {
   // Add click event listener to element
   element.addEventListener('click', () => {
@@ -56,8 +92,8 @@ function deleteData(element) {
       section.remove();
     });
   });
-// Usage
-deletEl.forEach(btn => {
-  deleteData(btn);
-});
+  // Usage
+  deletEl.forEach(btn => {
+    deleteData(btn);
+  });
 }
---
+++ test.js
@@ -0,0 +1,4 @@
+
+const names = "baba"
+const grett = `hello ${names}`
+console.log('grett: ', grett);
