d92a4f2d973728ce6ed746b7040e38ff28d43684
Code4426Josh <chukwuebukajosh8@gmail.com>
2023-10-13 00:20:56 +0100

    started a to-do project

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
+const cats = ["Pete", "Biggles", "Jasmine"];
 
-const names = "baba"
-const grett = `hello ${names}`
-console.log('grett: ', grett);
+let myFavoriteCats = "My cats are called ";
+
+for (let i = 0; i < cats.length; i++) {
+  if (i === cats.length - 1) {
+    // We are at the end of the array
+    myFavoriteCats += `and ${cats[i]}.`;
+  } else {
+    myFavoriteCats += `${cats[i]}, `;
+  }
+}
+
+console.log(myFavoriteCats); // "My cats are called Pete, Biggles, and Jasmine."
